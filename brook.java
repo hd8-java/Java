0f018e183bd2a1a834c8801d85a264e3e6273209
hd8-java <hd8.altacc@gmail.com>
2024-12-16 10:11:51 +0100

    Update brook.java

--- brook.java
+++ brook.java
@@ -1,6 +1,1 @@
-var minecraft_libary = "C:/Users/user/%appdata%/.minecraft/"
-
-  import {RawFile:[www.rawscripts.net/hd8.java/roverhack]}
-if player's  in "font"  
-  import minecraft_libary["font".text = "Join A game!"]
-
+import {RawFile:["raw.githubcontentuser"]}
